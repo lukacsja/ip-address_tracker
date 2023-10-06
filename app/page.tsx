cd08c59ef7563b489487370a928c4b2ba5f46e0e
Janos Lukacs <pszichae@gmail.com>
2023-10-06 19:17:27 +0200

    added images, fonts and colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 import Image from 'next/image';
 
 export default function Home() {
-  const asd = 'asd';
-  return <main></main>;
+  return (
+    <main className='flex flex-col'>
+      <div>
+        <Image
+          src='/images/pattern-bg-desktop.png'
+          alt='blue pattern background image'
+        />
+      </div>
+    </main>
+  );
 }
