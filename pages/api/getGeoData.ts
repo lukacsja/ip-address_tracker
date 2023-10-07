4f41f9a2964a0ad51e931ca9a50f21ffa664eadb
Janos Lukacs <pszichae@gmail.com>
2023-10-08 01:23:52 +0200

    deploy test

--- pages/api/getGeoData.ts
+++ pages/api/getGeoData.ts
@@ -7,26 +7,23 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    const response = await fetch('https://api.ipify.org?format=json');
+    const visitorIp = req.headers['x-forwarded-for'];
 
-    if (response.ok) {
-      const data = await response.json();
-      const visitorIp = data.ip;
+    if (!visitorIp) {
+      throw new Error('Visitor IP not found in headers');
+    }
 
-      const geoResponse = await fetch(
-        `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${visitorIp}`
-      );
-      if (geoResponse.ok) {
-        const geoData = await geoResponse.json();
+    const geoResponse = await fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${visitorIp}`
+    );
 
-        res.status(200).json(geoData);
-      } else {
-        res
-          .status(500)
-          .json({ error: 'Failed to fetch geolocation information' });
-      }
+    if (geoResponse.ok) {
+      const geoData = await geoResponse.json();
+      res.status(200).json(geoData);
     } else {
-      res.status(500).json({ error: 'Failed to fetch visitor information' });
+      res
+        .status(500)
+        .json({ error: 'Failed to fetch geolocation information' });
     }
   } catch (error) {
     console.error(error);
